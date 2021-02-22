b43d7499875d98434b1b5a214c568c807d1fa094
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-02-22 14:46:38 -0600

    Added Core Data Classes and objects
    Added NSPersistentContainer
    Added Save Context function
    Added CoreDataModel and defined my entity and the attributes.
    Added CoreData Model classes

---
+++ Core Data/Core Data/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  Core Data
+//
+//  Created by Alex Thompson on 2/22/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
---
+++ Core Data/Core Data/Model/Person+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Person+CoreDataProperties.swift
+//  Core Data
+//
+//  Created by Alex Thompson on 2/22/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Person {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
+        return NSFetchRequest<Person>(entityName: "Person")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var gender: String?
+    @NSManaged public var age: Int64
+
+}
+
+extension Person : Identifiable {
+
+}
